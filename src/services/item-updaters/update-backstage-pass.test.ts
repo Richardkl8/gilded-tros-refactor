03174ebcc947d7406ecdbfe72b3f8636655e4a51
RichardKrijnseLocker <richard@notmedior.nl>
2024-10-12 13:42:40 +0200

    refactor: added logic to update backstage passes

---
+++ src/services/item-updaters/update-backstage-pass.test.ts
@@ -0,0 +1,47 @@
+import { createItem } from '@models/item';
+import { updateBackstagePass } from '@services/item-updaters/update-backstage-pass';
+import { ITEMS } from '@constants/items';
+
+describe('updateBackstagePass', () => {
+  it('should decrease sellIn by 1', () => {
+    const item = createItem(ITEMS.BACKSTAGE_PASSES_REFACTOR, 15, 20);
+    const updatedItem = updateBackstagePass(item);
+
+    expect(updatedItem.sellIn).toBe(14);
+  });
+
+  it('should increase quality by 1 when sellIn is more than 10 days', () => {
+    const item = createItem(ITEMS.BACKSTAGE_PASSES_REFACTOR, 15, 20);
+    const updatedItem = updateBackstagePass(item);
+
+    expect(updatedItem.quality).toBe(21);
+  });
+
+  it('should increase quality by 2 when sellIn is 10 days or less', () => {
+    const item = createItem(ITEMS.BACKSTAGE_PASSES_REFACTOR, 10, 20);
+    const updatedItem = updateBackstagePass(item);
+
+    expect(updatedItem.quality).toBe(22);
+  });
+
+  it('should increase quality by 3 when sellIn is 5 days or less', () => {
+    const item = createItem(ITEMS.BACKSTAGE_PASSES_REFACTOR, 5, 20);
+    const updatedItem = updateBackstagePass(item);
+
+    expect(updatedItem.quality).toBe(23);
+  });
+
+  it('should drop quality to 0 after the event', () => {
+    const item = createItem(ITEMS.BACKSTAGE_PASSES_REFACTOR, 0, 20);
+    const updatedItem = updateBackstagePass(item);
+
+    expect(updatedItem.quality).toBe(0);
+  });
+
+  it('should not increase quality beyond 50', () => {
+    const item = createItem(ITEMS.BACKSTAGE_PASSES_REFACTOR, 5, 49);
+    const updatedItem = updateBackstagePass(item);
+
+    expect(updatedItem.quality).toBe(50);
+  });
+});
---
+++ src/services/item-updaters/update-backstage-pass.ts
@@ -0,0 +1,18 @@
+import { Item } from '@models/item';
+import { decrementSellIn, increaseQuality } from '@services/item-updaters/utils/item-updaters-utils';
+import { QUALITY } from '@constants/quality';
+
+export const updateBackstagePass = (item: Item): Item => {
+  const updatedItem = decrementSellIn(item);
+
+  if (updatedItem.sellIn < 0) {
+    return { ...updatedItem, quality: 0 };
+  }
+
+  let qualityIncreaseAmount = QUALITY.BACKSTAGE_PASS_INCREASE_DEFAULT;
+
+  if (updatedItem.sellIn < 10) qualityIncreaseAmount = QUALITY.BACKSTAGE_PASS_INCREASE_LESS_THAN_10_DAYS;
+  if (updatedItem.sellIn < 5) qualityIncreaseAmount = QUALITY.BACKSTAGE_PASS_INCREASE_LESS_THAN_5_DAYS;
+
+  return increaseQuality(updatedItem, qualityIncreaseAmount);
+};
