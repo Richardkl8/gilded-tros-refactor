a07262b07e70256663f693caf5a71bcd4a0c1e91
RichardKrijnseLocker <richard@notmedior.nl>
2024-10-12 14:12:53 +0200

    refactor: updated gilded-tros to work with new functions

---
+++ src/gilded-tros.test.ts
@@ -0,0 +1,23 @@
+import { createItem, Item } from '@models/item';
+import { GildedTros } from '@/gilded-tros';
+import { ITEMS } from '@constants/items';
+
+describe('GildedTros', () => {
+  it('should update quality for all items', () => {
+    const items: Item[] = [
+      createItem(ITEMS.GOOD_WINE, 5, 20),
+      createItem(ITEMS.BACKSTAGE_PASSES_REFACTOR, 15, 20),
+      createItem(ITEMS.DUPLICATE_CODE, 10, 20),
+      createItem('Normal Item', 10, 20),
+    ];
+    const gildedTros = GildedTros(items);
+    const updatedItems = gildedTros.updateQuality();
+
+    expect(updatedItems).toEqual([
+      createItem(ITEMS.GOOD_WINE, 4, 21),
+      createItem(ITEMS.BACKSTAGE_PASSES_REFACTOR, 14, 21),
+      createItem(ITEMS.DUPLICATE_CODE, 9, 18),
+      createItem('Normal Item', 9, 19),
+    ]);
+  });
+});
